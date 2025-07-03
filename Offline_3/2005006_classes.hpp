ceb73abfee8b31e13064de482a0f30227f38e2a9
Kowshik-006 <kowshik.saha.ks.ks@gmail.com>
2025-07-03 16:23:37 +0600

    Implemented data loading.

---
+++ Offline_3/2005006_classes.hpp
@@ -0,0 +1,75 @@
+class Vector{
+    public:
+    double x,y,z;
+    Vector() : x(0), y(0), z(0) {}
+    Vector(double x, double y, double z) : x(x), y(y), z(z) {}
+};
+class Color{
+    public:
+    double r, g, b;
+    Color() : r(0), g(0), b(0) {}
+    Color(double r, double g, double b) : r(r), g(g), b(b) {}
+};
+class CoEfficients{
+    public: 
+    double ambient, diffuse, specular, reflection;
+    CoEfficients() : ambient(0), diffuse(0), specular(0), reflection(0) {}
+    CoEfficients(double ambient, double diffuse, double specular, double reflection) 
+        : ambient(ambient), diffuse(diffuse), specular(specular), reflection(reflection) {}
+};
+class Object{
+    public:
+    Vector reference_point;
+    double height, width, length;
+    Color color;
+    CoEfficients coefficients;
+    int shine;
+    Object(){}
+    virtual void draw() {}   
+    void setColor(Color color) {
+        this->color = color;
+    }
+    void setShine(int shine) {
+        this->shine = shine;
+    }
+    void setCoefficients(CoEfficients coefficients) {
+        this->coefficients = coefficients;
+    }
+};
+
+
+class Sphere : public Object {
+    public: 
+    Sphere(Vector center, double radius){
+        reference_point = center;
+        length = radius;
+    }
+    void draw();
+};
+
+class Triangle : public Object {
+    public:
+    Vector a, b, c;
+    Triangle(Vector a, Vector b, Vector c) : a(a), b(b), c(c) {}
+    void draw();
+};
+
+class Light{
+    public:
+    Vector light_position;
+    Color color;
+    Light(Vector position, Color color) : light_position(position), color(color){}
+};
+
+class PointLight : public Light{
+    public:
+    PointLight(Vector position, Color color) : Light(position, color){}
+};
+
+class SpotLight : public Light {
+    public:
+    Vector light_direction;
+    double cutoff_angle;
+    SpotLight(Vector position, Color color, Vector direction, double cutoff_angle) 
+        : Light(position, color), light_direction(direction), cutoff_angle(cutoff_angle){}
+};
---
+++ Offline_3/2005006_main.cpp
@@ -0,0 +1,117 @@
+#include "2005006_classes.hpp"
+#include <bits/stdc++.h>
+#include<GL/glut.h>
+
+using namespace std;
+
+vector<Object> objects;
+vector<Light> lights;
+
+string scene_file = "scene.txt";
+int recursion_levels;
+int image_width;
+int image_height;
+
+void Sphere :: draw(){
+    // lalala
+}
+void Triangle :: draw(){
+    // lalala
+}
+
+void load_data(){
+    ifstream input_file(scene_file);
+    if (!input_file.is_open()) {
+        cerr << "Error opening file: " << scene_file << endl;
+        return;
+    }
+
+    input_file >> recursion_levels >> image_width;
+    image_height = image_width;
+
+    int num_objects, num_point_lights, num_spot_lights;
+    input_file >> num_objects;
+
+    for(int i=0; i<num_objects; i++){
+        string object_type;
+        input_file >> object_type;
+
+        Color color;
+        CoEfficients coeff;
+        int shine;
+        Object obj;
+
+        if(object_type == "sphere"){
+            Vector center;
+            double radius;
+            
+            input_file >> center.x >> center.y >> center.z
+                       >> radius;
+                       
+            obj = Sphere(center, radius);
+        }
+        else if(object_type == "triangle"){
+            Vector a, b, c;
+            input_file >> a.x >> a.y >> a.z
+                       >> b.x >> b.y >> b.z
+                       >> c.x >> c.y >> c.z;
+            obj = Triangle(a,b,c);
+        }
+
+        else if(object_type == "general"){
+            double A,B,C,D,E,F,G,H,I,J;
+            Vector ref_point;
+            double height, width, length;
+            input_file >> A >> B >> C >> D >> E >> F >> G >> H >> I >> J
+                       >> ref_point.x >> ref_point.y >> ref_point.z >> height >> width >> length; 
+        }
+        else{
+            cerr << "Invalid  object type : " << object_type << endl;
+            return;
+        }
+
+        input_file >> color.r >> color.g >> color.b
+                   >> coeff.ambient >> coeff.diffuse >> coeff.specular >> coeff.reflection
+                   >> shine;
+        
+        obj.setColor(color);
+        obj.setCoefficients(coeff);
+        obj.setShine(shine);
+
+        objects.push_back(obj);
+    }
+
+    input_file >> num_point_lights;
+
+    for(int i=0; i<num_point_lights; i++){
+        Vector position;
+        Color color;
+        input_file >> position.x >> position.y >> position.z
+                   >> color.r >> color.g >> color.b;
+        
+        lights.push_back(PointLight(position, color));
+    }
+
+    input_file >> num_spot_lights;
+
+    for(int i=0; i<num_spot_lights; i++){
+        Vector position, direction;
+        Color color;
+        double cutoff_angle;
+
+        input_file >> position.x >> position.y >> position.z
+                   >> color.r >> color.g >> color.b
+                   >> direction.x >> direction.y >> direction.z
+                   >> cutoff_angle;
+                   
+        lights.push_back(SpotLight(position, color, direction, cutoff_angle));
+    }
+
+    input_file.close();
+    cout << "Successfully loaded data from " << scene_file << endl;
+}
+
+int main(){
+    load_data();
+    return 0;
+}
